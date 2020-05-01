07a2f9b3199d3304df232af5fc2c66a57e1a6167
Tayfun Sur <tayfunsur@gmail.com>
2020-05-01 18:56:49 +0300

    career list view and controller function are written

--- controllers/careerPathControllers.js
+++ controllers/careerPathControllers.js
@@ -1,17 +1,23 @@
 const CareerPath = require('../models/career_path');
 
+exports.careerpath_list = (req, res, next) => {
+  CareerPath.find((err, results) =>{
+    if (err){return next(err)}
+    res.render('careerpath_list', {title: 'All Careers Are Listed Here', data:results})
+    console.log(results)
+  })
+ 
+}
+exports.careerpath_detail = (req, res) => {
+  res.send('careerpath detail'+this._id)
+}
 exports.careerpath_create_get = (req, res) => {
   res.send('careerpath create get')
 }
 exports.careerpath_create_post = (req, res) => {
   res.send('careerpath create post')
 }
-exports.careerpath_list = (req, res) => {
-  res.send('career path list will be implemented')
-}
-exports.careerpath_detail = (req, res) => {
-  res.send('careerpath detail'+this._id)
-}
+
 exports.careerpath_update_get = (req, res) => {
   res.send('careerpath update get')
 }
