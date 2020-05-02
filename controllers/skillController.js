082e755ced55fa8bd6f84d5f3c8770663107f239
Tayfun Sur <tayfunsur@gmail.com>
2020-05-02 12:43:53 +0300

    skill from bugs are fixed & create skill post cont. func. is added

--- controllers/skillController.js
+++ controllers/skillController.js
@@ -3,6 +3,9 @@ const CareerPath = require('../models/career_path');
 
 const async = require('async');
 
+const { body, validationResult } = require('express-validator/check');
+const { sanitizeBody } = require('express-validator/filter');
+
 exports.index = function (req, res) {
   async.parallel(
     {
@@ -35,7 +38,6 @@ exports.skill_list = (req, res, next) => {
         title: 'All The Skills You Need Are Only One Click Away!',
         data: results,
       });
-      console.log(results);
     });
 };
 
@@ -55,12 +57,60 @@ exports.skill_create_get = (req, res, next) => {
     if (err) {
       return next(err);
     }
-    res.render('skill_form', { title: 'New Skill Create Form', data: results });
+    res.render('skill_form', { title: 'New Skill Create Form', careerPath: results });
   });
 };
-exports.skill_create_post = (req, res) => {
-  res.send('skill create post');
-};
+exports.skill_create_post = [
+  //validate
+  body('name', 'name must be specified').trim().isLength({ min: 1 }),
+  body('description', 'description must be specified')
+    .trim()
+    .isLength({ min: 2 }),
+  body('career_path', 'career path must be between 0 and 10000')
+    .trim()
+    .isLength({ min: 1 }),
+  body('price', 'price must be specified').trim().isInt({ gt: 0, lt: 10000 }),
+  body('numInStock', 'stock number must be between 0 and 1000')
+    .trim()
+    .isInt({ gt: 0, lt: 1000 }),
+
+  sanitizeBody('*').escape(),
+
+  (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    // Create a Book object with escaped and trimmed data.
+    var skill = new Skill({
+      name: req.body.name,
+      description: req.body.description,
+      career_path: req.body.career_path,
+      price: req.body.price,
+      numInStock: req.body.numInStock,
+    });
+    
+    if (!errors.isEmpty()) {
+      CareerPath.find({}).exec((err, results) => {
+        if (err) {
+          return next(err);
+        }
+        
+        res.render('skill_form', {
+          title: 'New Skill Create Form Again',
+          careerPath: results,
+          skill: skill,
+          errors: errors.array()
+        });
+      });
+      return;
+    } else {
+      skill.save(function (err){
+        if (err) {return next(err)}
+        res.redirect(skill.url)
+      })
+    }
+  },
+];
 exports.skill_delete_get = (req, res, next) => {
   Skill.findById(req.params.id).exec((err, results) => {
     if (err) {
@@ -76,18 +126,26 @@ exports.skill_delete_post = (req, res) => {
   res.send('skill delete post');
 };
 exports.skill_update_get = (req, res, next) => {
-  async.parallel({
-    skill: (callback) =>{
-      Skill.findById(req.params.id).exec(callback)
+  async.parallel(
+    {
+      skill: (callback) => {
+        Skill.findById(req.params.id).exec(callback);
+      },
+      careerPaths: (callback) => {
+        CareerPath.find({}, 'name').exec(callback);
+      },
     },
-    careerPaths: (callback)=>{
-      CareerPath.find({}, 'name').exec(callback)
+    (err, results) => {
+      if (err) {
+        return next(err);
+      }
+      res.render('skill_form', {
+        title: 'update the skill',
+        skill: results.skill,
+        careerPaths: results.careerPaths,
+      });
     }
-  }, 
-  (err, results)=>{
-    if (err){return next(err)}
-    res.render('skill_form', {title: 'update the skill', skill:results.skill, careerPaths: results.careerPaths})
-  })
+  );
 };
 exports.skill_update_post = (req, res) => {
   res.send('skill update post');
